edfda4c6ca9491bc3b38b3ac742bd9c76578541c
aliradmanesh <ali.radmanesh1995@gmail.com>
2024-07-11 21:35:38 +0330

    feat: change structure of json for sending in apis

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -8,21 +8,21 @@ export async function POST(request) {
     const { email, password } = await request.json()
     
     if (!email || !password) 
-      return NextResponse.json({ message: "Both fields are required!" }, { status: 400 })
+      return NextResponse.json({ status: 400, message: "Both fields are required!" })
 
     const user = await prisma.user.findFirst({
       where: { email: email.toLowerCase() }
     })
 
-    if (!user) return NextResponse.json({ message: "user not found!" }, { status: 400 })
+    if (!user) return NextResponse.json({ status: 400, message: "user not found!" })
 
     if (await bcrypt.compare(password, user.password)) {
       const { password: hashedPassword, ...result } = user
       const accessToken = signJwtAccessToken(result)
-      return NextResponse.json({ result: { ...result, accessToken } }, { status: 200 })
-    } else return NextResponse.json({ message: "Password incorrect" }, { status: 400 })
+      return NextResponse.json({ status: 200, result: { ...result, accessToken } })
+    } else return NextResponse.json({ status: 400, message: "Password incorrect" })
   } catch (error: any) {
     console.error(error)
-    return NextResponse.json({ result: error, message: "Something went wrong while trying to log in" }, { status: 500 })
+    return NextResponse.json({ status: 500, result: error, message: "Something went wrong while trying to log in" })
   }
 }
--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -7,7 +7,7 @@ export async function POST(request) {
     const { email, password } = await request.json()
     
     if (!email || !password) 
-      return NextResponse.json({ message: "Something went wrong while trying to register" }, { status: 400 })
+      return NextResponse.json({ status: 400, message: "Something went wrong while trying to register" })
 
     const user = await prisma.user.create({
       data: {
@@ -17,9 +17,9 @@ export async function POST(request) {
     })
 
     const { password: hashedPassword, ...result } = user
-    return NextResponse.json({ result }, { status: 201 })
+    return NextResponse.json({ status: 201, result })
   } catch (error: any) {
     console.error(error)
-    return NextResponse.json({ result: error, message: "Something went wrong while trying to register" }, { status: 500 })
+    return NextResponse.json({ status: 500, result: error, message: "Something went wrong while trying to register" })
   }
 }
