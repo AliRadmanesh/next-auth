8cf2fe174ac423e636811cf3b503335425eff553
aliradmanesh <ali.radmanesh1995@gmail.com>
2024-07-11 21:35:15 +0330

    fix: change sign up to register in button

--- components/RegisterForm.tsx
+++ components/RegisterForm.tsx
@@ -34,7 +34,7 @@ const RegisterForm = () => {
     <form onSubmit={handleSubmit} className="flex flex-col space-y-12 w-full px-32">
       <input type="text" value={email} onChange={handleEmailChange} placeholder="Enter your email address" className="border-b border-b-gray-200 hover:border-b-gray-500" />
       <input type="password" value={password} onChange={handlePasswordChange} placeholder="Enter your password" className="border-b border-b-gray-200 hover:border-b-gray-500" />
-      <button type="submit" className="border rounded-lg px-6 py-2 bg-gray-100 hover:bg-gray-200 duration-300 uppercase text-sm">Sign up</button>
+      <button type="submit" className="border rounded-lg px-6 py-2 bg-gray-100 hover:bg-gray-200 duration-300 uppercase text-sm">Register</button>
       {error && (<p className="text-red-500 font-bold text-center">{error}</p>)}
     </form>
   )
