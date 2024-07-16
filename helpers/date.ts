4da59fa796f83979063521f8d2a24b63f77b2581
aliradmanesh <ali.radmanesh1995@gmail.com>
2024-07-16 12:00:53 +0330

    feat: add date difference function

--- helpers/date.ts
+++ helpers/date.ts
@@ -3,4 +3,18 @@ import dayjs, { ManipulateType } from 'dayjs';
 const someTimeLater = ({ value, unit }: { value: number; unit?: ManipulateType }) =>
   dayjs().add(value, unit).toISOString();
 
-export { someTimeLater };
+const differenceBetweenDates = ({
+  firstDate,
+  secondDate,
+  unit = 'second',
+}: {
+  firstDate: string;
+  secondDate: string;
+  unit?: ManipulateType;
+}) => {
+  const laterDate = dayjs(secondDate);
+  const soonerDate = dayjs(firstDate);
+  return laterDate.diff(soonerDate, unit);
+};
+
+export { someTimeLater, differenceBetweenDates };
